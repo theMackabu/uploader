6c3657b3f75511660c2e30b2dc013378a8fea524
theMackabu <theMackabu@gmail.com>
2025-06-30 21:59:52 -0700

    feat(server): improve error handling by adding specific HTTP exception cases

--- pkg/server.ts
+++ pkg/server.ts
@@ -1,4 +1,6 @@
 import { PORT } from '@/env';
+import { HTTPException } from 'hono/http-exception';
+
 import { cdn } from '@/routes';
 import { init } from '@/database';
 import { logger } from 'hono/logger';
@@ -10,9 +12,13 @@ server.route('/', cdn);
 
 server.notFound(c => c.text('not found :(', 404));
 
-server.onError((err, res) => {
+server.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return c.text(err.message, err.status);
+  }
+
   console.error(err);
-  return res.text(err.message, 500);
+  return c.text(err.message, 500);
 });
 
 export default {
